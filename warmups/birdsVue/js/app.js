3cd5053315da17568940b289c8c057b24715e0b7
Travis Jette <travisjette@Traviss-MacBook-Pro.local>
2017-06-30 12:09:03 -0600

    added more birds using constructor function and using that data in vue component

--- warmups/birdsVue/js/app.js
+++ warmups/birdsVue/js/app.js
@@ -15,15 +15,35 @@ var b2 = {
 console.log(b1);
 console.log(b2);
 
-function Bird(name, species, gender, img){
+function Bird(name, species, gender, img, predator){
   this.name = name;
   this.species = species;
   this.gender = gender;
   this.img = img;
+  this.predator = predator ? true : false;
 };
 
 var b3 = new Bird('robin', 'bird', 'male', '')
 var b4 = new Bird('something', 'bird', 'female', '')
+var b5 = new Bird('penguin', 'african', 'male', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-dl2WX2fiXXOH3UmyV1DeumHTLfqOgT72ZhmRlk2BZkv7Na-UhQ');
+var b6 = new Bird('parrot', 'cockatiel', 'female', '');
+var b7 = new Bird('osprey', 'birds', 'female', '', true);
 
-console.log(b3);
-console.log(b4);
+
+console.log(b3,b4,b5,b6,b7);
+var title = "Birds Collection";
+var birds = new Array()
+birds.push(b3,b4,b5,b6,b7);
+console.log(birds);
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    title: title,
+    birds: birds,
+    name: undefined,
+    species: undefined,
+    gender: undefined,
+    img: undefined
+  }
+});
